a7838cefbf5b819031cd5217b3331e5851a68b53
Gabriel Romanini de Barros <gabriel.romanini.barros@gmail.com>
2025-02-20 22:57:46 -0500

    Type mismatch bug fixes

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,11 @@
-use sqlx::{postgres::PgPoolOptions, query_scalar, query};
+use sqlx::{postgres::PgPoolOptions, query_scalar, query, query_as};
 mod models;
 use models::{User, Auth};
 use dotenv::dotenv;
 
 impl Auth {
 
-    pub async fn new_connection() -> Result<Self, sqlx::Error> {
+    pub async fn new_connection(&self) -> Result<Self, sqlx::Error> {
         dotenv().ok();
         let db_url = std::env::var("DATABASE_URL")
             .expect("DATABASE_URL must be set");
@@ -14,11 +14,15 @@ impl Auth {
         let db = PgPoolOptions::new()
             .connect(&db_url)
             .await?;
-        Ok(Self { db })
+
+        let auth = Self {db};
+        auth.setup().await?;
+
+        Ok(auth)
     }
 
     async fn setup(&self) -> Result<(), sqlx::Error> {
-        let table_exists = sqlx::query_scalar!(
+        let table_exists = query_scalar!(
             r#"
             SELECT EXISTS (
                 SELECT 1 
@@ -33,7 +37,7 @@ impl Auth {
 
         if !table_exists {
             // Create users table
-            sqlx::query!(
+            query!(
                 r#"
                 CREATE TABLE users (
                     id SERIAL PRIMARY KEY,
@@ -49,6 +53,44 @@ impl Auth {
         }
 
         Ok(())
+    }
+
+    pub async fn register_user(&self, username: &str, password: &str, email: &str) -> Result<User, sqlx::Error> {
+        if self.user_exists(username, email).await? {
+            Err::<(), sqlx::Error>(sqlx::Error::RowNotFound);
+        }
+        // Insert user and return all fields
+        query_as!(
+            User,  // This will return the created user
+            r#"
+            INSERT INTO users (username, email, password, created_at)
+            VALUES ($1, $2, $3, CURRENT_TIMESTAMP)
+            RETURNING id, username, email, password, created_at
+            "#,
+            username,
+            email,
+            password,
+        )
+        .fetch_one(&self.db)
+        .await
+            
+    }
+
+    async fn user_exists(&self, username: &str, email: &str) -> Result<bool, sqlx::Error> {
+        let exists = sqlx::query_scalar!(
+            r#"
+            SELECT EXISTS (
+                SELECT 1 FROM users 
+                WHERE username = $1 OR email = $2
+            )
+            "#,
+            username,
+            email
+        )
+        .fetch_one(&self.db)
+        .await?;
+
+        Ok(exists.unwrap_or(false))
     }
 
 }
--- src/models.rs
+++ src/models.rs
@@ -1,3 +1,5 @@
+use chrono::{DateTime, Utc};
+
 #[derive(Debug, Clone, sqlx::FromRow)]
 pub struct User {
     pub id: i32,
