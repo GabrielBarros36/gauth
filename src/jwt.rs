333ad34e63346a1d49d9979ddf52c73e98547d23
Gabriel Romanini de Barros <gabriel.romanini.barros@gmail.com>
2025-03-02 23:10:33 -0500

    Unit test - jwt validation

--- src/jwt.rs
+++ src/jwt.rs
@@ -4,13 +4,13 @@ use dotenv::dotenv;
 use crate::models::{Claims, User};
 
 // Move .env usage to main runtime later
-pub async fn issue_token(user: User) -> String {
+pub async fn issue_token(user: &User) -> String {
 	dotenv().ok();
 	let key = std::env::var("JWT_SECRET").unwrap();
 
 	let claim = Claims {
-		sub: user.username,
-		exp: 100000000,
+		sub: user.username.clone(),
+		exp: 1000000000000000, // REMINDER - without iat this counts from Unix epoch
 		//iat: chrono::offset::Utc::now().into(),
 	};
 
@@ -22,17 +22,43 @@ pub async fn issue_token(user: User) -> String {
     token
 }
 
+// Should probably do away w/ this
 // Returns true if token is valid
 // Should probably move the algorithm object to runtime later
-// Seems like as of now tokens don't expire? 
-pub async fn validate_token(user: User, token: String) -> bool {
+pub async fn validate_token(token: &String) -> bool {
 	dotenv().ok();
 	let key = std::env::var("JWT_SECRET").unwrap();
 
 	let mut validation = Validation::new(Algorithm::HS256);
 
 	match decode::<Claims>(&token, &DecodingKey::from_secret(key.as_bytes()), &validation) {
-		Err(e) => false,
+		Err(e) => {dbg!(e); false},
 		Ok(_) => true,
 	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+
+	#[cfg_attr(feature = "jwt-tests", tokio::test)]
+	async fn issue_and_validate() {
+
+		let user = User {
+            id: None,
+            username : "test".to_string(),
+            email: "test@test.com".to_string(),
+            password: "test".to_string(),
+            created_at: None,
+
+        };
+
+        let token : String = issue_token(&user).await;
+        let validation: bool = validate_token(&token).await;
+        dbg!(validation);
+        assert_eq!(validation, true);
+
+	}
+
 }
